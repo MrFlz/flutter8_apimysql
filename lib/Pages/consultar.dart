f6d51427a5625aa1a6c8adeed29ae045fbe2fa39
flonoalemil@gmail.com <flonoalemil@gmail.com>
2021-10-17 14:57:29 -0500

    menú con consultar - filtrar por qr

---
+++ lib/Pages/consultar.dart
@@ -0,0 +1,211 @@
+import 'dart:convert';
+//import 'dart:html';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'package:flutter_api_mysql/Models/Rol.dart';
+import 'package:http/http.dart' as http; //encapsula paquete importado en un objeto para ser fácil su acceso y manipulación
+
+import 'package:barcode_scan/barcode_scan.dart';
+
+class Consulta extends StatefulWidget {
+  Consulta({Key? key}) : super(key: key);
+  
+  @override
+  State<Consulta> createState() => _ConsultaState();
+}
+
+class _ConsultaState extends State<Consulta>{
+  ScanResult ? _scanResult;
+
+  final ButtonStyle _style = ElevatedButton.styleFrom( //estilo del botón (pueden usarlo varios botones y crearse varios styles)
+    textStyle: const TextStyle(
+      fontSize: 20
+    )
+  );
+  final _tectrl_tf_roleid = TextEditingController(); // Crea un controlador de texto. Lo usaremos para recuperar el valor actual del TField
+  //late Future<List<Rol>> _futlistRoles;
+  String apiurl="http://192.168.100.43:5000/role/aget/";
+  var item;
+  var qrcontent=0;  
+
+  List<Widget> _listRoles(data){
+    List<Widget> roles = [];
+    for (var rol in data){
+      roles.add(
+        Card(
+          child: Column(
+            children: [
+              Text(rol.rid.toString()),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(rol.rname),
+              )
+            ]
+          )
+        )
+      );
+    }
+    return roles;    
+  }
+
+  Future<List<Rol>> _getRoles() async {
+    final response = await http.get(Uri.parse(apiurl));
+    List<Rol> roles = [];
+
+    if(response.statusCode == 200){
+      String body = utf8.decode(response.bodyBytes);
+      final jsonData = jsonDecode(body);
+
+      // print(jsonData["data"][0]["id"]); // ejemplo sobre la obtención de un dato a lo largo del json
+
+      //agregar objetos de tipo rol en la lista creada anteriormente
+      for ( item in jsonData){        
+        roles.add(
+          Rol(item["roleid"], item["rolename"])
+        );
+      }
+      return roles;
+    }else{
+      throw Exception ("Algo fallo en la conexión (creeeo xd )");
+    }  
+  }
+
+  Future<List<Rol>> _getRoles_byId() async {
+    final response = await http.get(Uri.parse(apiurl));
+    List<Rol> roles = [];
+
+    if(response.statusCode == 200){
+      String body = utf8.decode(response.bodyBytes);
+      final jsonData = jsonDecode(body);
+
+      //filtrar y ****agregar objetos de tipo rol en la lista creada anteriormente
+      for ( item in jsonData){
+        if(item["roleid"]==qrcontent){
+          roles.add(
+            Rol(item["roleid"], item["rolename"])
+          );
+          break;
+        }       
+      }
+      return roles;
+    }else{
+      throw Exception ("Algo fallo en la conexión (creeeo xd )");
+    }  
+  }
+
+  Future<void> _ft_qr_scan() async {
+    var result = await BarcodeScanner.scan();
+    setState(() {
+      _scanResult = result; //asiga el resultado directo del escaneo al tipo de resultado que contiene métodos para obtener la data obtenida del 1er resultado 
+      qrcontent = int.parse(_scanResult!.rawContent); //el resultado es un string, pero el qrcontent es int  
+    });
+  }
+
+  @override
+  void initState() { // función propia de Flutter que se ejecuta al abrirse una ventana, es lo primero que se ejecuta
+    super.initState();   
+    //_futlistRoles = _getRoles();
+  }
+
+  @override
+  void dispose() {
+    // Limpia el controlador cuando el Widget se descarte
+    _tectrl_tf_roleid.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      title: 'flutter_api_mysql',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('API dotnet + mysql'),
+        ),
+        body: cuerpo(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: (){
+            _ft_qr_scan(); //se ejecuta el metodo future scann
+          },
+          tooltip: 'Escanear',
+          child: const Icon(Icons.qr_code),
+        )
+      )
+    );
+  }
+
+  // INIT PERSONAL WIDGETS <==================
+  Widget cuerpo(){
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              _tf_roleid(),
+              const SizedBox(height: 10),
+              _bt_findbyid()
+            ]
+          ),
+          _ftbdr_lv_roleslist(),
+          Text(qrcontent.toString())
+          /*_scanResult==null ? const Text('Waiting data...'): 
+            Text('Contenido: ${_scanResult!.rawContent}')*/
+        ]
+      )
+    );
+  }
+
+  Widget _ftbdr_lv_roleslist(){
+    return FutureBuilder (
+      future:  _scanResult==null ? _getRoles() : _getRoles_byId(), //si no hay escaneo, muestra todo, si hay, filtra
+      builder: (context, snapshot) {
+        if (snapshot.hasData){   
+          return Expanded(
+            child: ListView(
+              children: _listRoles(snapshot.data)
+            )
+          );
+        }else if(snapshot.hasError){
+          print(snapshot.error.toString() + "<=== este es el error cachado" );
+          return const Text("errorzaso");
+        }
+        return const Center(
+          child: CircularProgressIndicator()            
+        );
+      },        
+    );
+  }
+  
+  Widget _tf_roleid(){
+    return Expanded(  //limita el tamaño de expansión del textfield (oséase: ancho)       
+      child: TextField(
+        inputFormatters: <TextInputFormatter>[
+          FilteringTextInputFormatter.digitsOnly,
+        ],
+        keyboardType: const TextInputType.numberWithOptions(),
+        controller: _tectrl_tf_roleid, //asociamos el TextEditingController como propiedad del TextField
+        decoration: const InputDecoration( //preferible const por los datos que maneja
+          hintText: "Filtrar by id",
+          fillColor: Colors.white,
+          filled: true
+        )
+      )
+    );
+  }
+
+  Widget _bt_findbyid(){
+    return ElevatedButton(
+      style: _style,
+      onPressed: (){        
+        //_getRoles(); //validar si algo hace? :/ 
+        print(_tectrl_tf_roleid.text); //se recupera el valor del controller asociado al textField y shá uwu
+      }, 
+      child: const Text("Buscar")          
+    );
+  } 
+  //END PERSONAL WIDGETS <===================
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,139 +1,68 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter_api_mysql/Pages/consultar.dart';
 
-import 'package:flutter_api_mysql/Models/Rol.dart';
-import 'package:http/http.dart' as http; //encapsula paquete importado en un objeto para ser fácil su acceso y manipulación
-
-void main() => runApp(const MyApp());
-
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  State<MyApp> createState() => _MyAppState();
+void main() {
+  runApp(const MyApp());
 }
 
-class _MyAppState extends State<MyApp> {
-
-  late Future<List<Rol>> _futlistRoles;
-
-  String apiurl="http://192.168.100.43:5000/role/aget/";
-  var item;
-
-  Future<List<Rol>> _getRoles() async {
-    final response = await http.get(Uri.parse(apiurl));
-  
-    List<Rol> roles = [];
-
-    if(response.statusCode == 200){
-      String body = utf8.decode(response.bodyBytes);
-
-      final jsonData = jsonDecode(body);
-
-      // print(jsonData["data"][0]["id"]); // ejemplo sobre la obtención de un dato a lo largo del json
-
-      //agregar objetos de tipo rol en la lista creada anteriormente
-      for ( item in jsonData){
-        roles.add(
-          Rol(item["roleid"], item["rolename"])
-        );
-      }
-      return roles;
-
-    }else{
-      throw Exception ("Algo fallo en la conexión (creeeo xd )");
-    }  
-  }
-
-  @override
-  void initState() { // función propia de Flutter que se ejecuta al abrirse una ventana, es lo primero que se ejecuta
-    super.initState();   
-    _futlistRoles = _getRoles() ;
-  }
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'flutter_api_mysql',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('API dotnet + mysql'),
+      title: 'API dotnet + mysql',
+      theme: ThemeData( 
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'api CRUD mysql + navigator'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [   
+            const Text("¿Qué desea hacer?"),   
+            const SizedBox(
+              height: 15.0,
+              width: 15.0,
+            ),         
+            eb_consultar()
+          ],
         ),
-        body: FutureBuilder (
-          future: _futlistRoles,
-          builder: (context, snapshot) {
-            if (snapshot.hasData){
-              print(snapshot.data);
-              return ListView(
-                children: _listRoles(snapshot.data)
-              );
-
-            }else if(snapshot.hasError){
-              print(snapshot.error.toString() + "<=== este es el error cachado" );
-              return const Text("errorzaso");
-
-            }
-            return const Center(
-              child: CircularProgressIndicator()                
-            );
-          },        
-        )
-        
-        /*Center(
-          // ignore: deprecated_member_use
-          child: RaisedButton(
-            child: const Text("Get API"),
-              onPressed: ()=>{ //para que funcione, el Center no debe ser CONST
-                // ignore: avoid_print
-                print("Presionaste get api"),
-                _listadoGifs = _getGifs(),
-                _listGifs( item["images"]["downsized"]["url"])
-              }
-          )
-        )*/
       )
     );
   }
 
-  List<Widget> _listRoles(data){
-    List<Widget> roles = [];
-
-    for (var rol in data){
-      roles.add(
-        Card(
-          child: Column(
-            children: [
-              Text(rol.rid.toString()),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(rol.rname),
-              )
-            ]
-          )
+  Widget eb_consultar(){
+    return ElevatedButton(
+      child: const Text("Consultar"),
+      onPressed: ()=>{                
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context)=> Consulta() ),                                     
         )
-      );
-    }
-
-    return roles;    
+      }
+    );
   }
-  
-  // PARA UNA MEJOR VISUALIZACION o algo así jejep... (NOTA: código incompleto)
-  /*List<Widget> _listGifs(List<Gif> data){
-    List<Widget> gifs = [];
-
-    for (var gif in data){
-      gifs.add(
-        Card(
-          child: Column(
-            children: [
-              Expanded(child: Image.network(gif.url, fit: BoxFit.fill))
-            ]
-          )
-        )
-      );
-    }
-
-    return gifs;    
-  }*/
-
 }
